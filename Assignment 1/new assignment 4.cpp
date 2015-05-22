6cabb32b95ddd580abcb27fd9816406c8c893f67
qrapheneee <grapheneee@gmail.com>
2015-05-22 07:02:37 +0100

    uploaded

---
+++ Assignment 1/new assignment 4.cpp
@@ -0,0 +1,72 @@
+/*
+ *  Question4.cpp
+ *
+ *  Name:           [ Inim Andrew Esiet ]
+ *  Department:     [ Computer science]
+ *  Matric. No:     [ 47123818DC ]
+ *
+ *  Program that inputs a five-digit integer,
+ *  separates the integer into its digits and prints them separated by three spaces each
+ *
+ *  Stub file to enable you complete assignment #1 - question #4
+ */
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    // Variable declaration
+    int digits;
+    
+    // display welcome messages to user
+    cout << "Welcome - This program reads a five-digit integer\n";
+    cout << "separates the integer into its digits and prints them\n";
+    cout << "separated by three spaces each\n\n";
+    int fivedigit;
+	int a,b,c,d,e;
+	
+	cout << "enter the five digit value: ";
+	cin >> fivedigit;
+	
+	if(fivedigit < 10000){
+		cout << " New form\t";
+	}
+	
+	if(fivedigit > 9999)
+	{
+		cout << "New form\t";
+	}
+	
+	a= fivedigit/10000;
+	b= (fivedigit/1000)%10;
+	c= (fivedigit/100)%10;
+	d= (fivedigit/10)%10;
+	e= fivedigit%10;
+	
+	cout << a;
+	cout << " " << b;
+	cout << " " << c;
+	cout << " " << d;
+	cout << " " << e;
+	
+    // TODO: Read integer using cin
+    cout << "Enter five digit integer value -  ";
+
+    
+    // check if digit is a five digit integer
+    if (to_string(digits).length() < 5 || to_string(digits).length() > 5)
+    {
+        cout << "\nERROR - Please enter a five digit integer";
+    } else {
+        // TODO: Separate the integer into its digits and prints them separated by three spaces each
+        // Put code here ...
+    
+    }
+    
+    cout << "\nThanks and Goodbye";
+    
+    //system("PAUSE");
+    return EXIT_SUCCESS;
+}

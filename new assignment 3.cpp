0aefd19a3f40a7fafbb088725f381b8a82e9541b
qrapheneee <grapheneee@gmail.com>
2015-05-22 06:57:53 +0100

    uploaded

---
+++ new assignment 3.cpp
@@ -0,0 +1,58 @@
+/*
+ *  Question3.cpp
+ *
+ *  Name:           [ Inim Andrew Esiet]
+ *  Department:     [ Computer Science ]
+ *  Matric. No:     [ 47123818DC ]
+ *
+ *  Program that prints a box, an oval, an arrow and a diamond
+ *
+ *  Stub file to enable you complete assignment #1 - question #3
+ */
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  cout<<"************\n";
+cout<<"*          *\n";
+cout<<"*          *\n";
+cout<<"*          *\n";
+cout<<"*          *\n";
+cout<<"*          *\n";
+cout<<"*          *\n";
+cout<<"************\n\n";
+cout<<"   ***\n";
+cout<<" *     *\n";
+cout<<"*       *\n";
+cout<<"*       *\n";
+cout<<"*       *\n";
+cout<<"*       *\n";
+cout<<"*       *\n";
+cout<<" *     *\n";
+cout<<"   ***\n\n";
+cout<<"    *\n";
+cout<<"   ***\n";
+cout<<"  *****\n";
+cout<<"    *\n";
+cout<<"    *\n";
+cout<<"    *\n";
+cout<<"    *\n";
+cout<<"    *\n\n";
+cout<<"    *\n";
+cout<<"   * *\n";
+cout<<"  *   *\n";
+cout<<" *     *\n";
+cout<<"*       *\n";
+cout<<" *     *\n";
+cout<<"  *   *\n";
+cout<<"   * *\n";
+cout<<"    *\n";
+    
+    
+    cout << "\nThanks and Goodbye";
+    
+    //system("PAUSE");
+    return EXIT_SUCCESS;
+}
